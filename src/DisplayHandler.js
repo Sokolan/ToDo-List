de2dbc0fb042d8548575ecd9cf265d4cddeb1512
Sokolan <valerysokolan@gmail.com>
2023-05-10 18:19:19 +0300

    working from terminal

--- src/DisplayHandler.js
+++ src/DisplayHandler.js
@@ -1,11 +1,18 @@
-export default class DisplayHandler {
-  #createHeader() {
-    const header = document.createElement('div');
-    header.
-  }
-  createDisplay() {
-    createHeader();
-    createBody();
-    createFooter();
-  }
+const createHeader = () => {
+  const header = document.createElement('div');
+  header.classList.add('header');
+
+  const logo = document.createElement('div');
+  logo.classList.add('logo');
+  logo.textContent = 'ToDo';
+
+  header.appendChild(logo);
+
+  return header;
+}
+createDisplay() {
+  const content = document.querySelector('#content');
+  content.appendChild(createHeader());
+  createBody();
+  createFooter();
 }
